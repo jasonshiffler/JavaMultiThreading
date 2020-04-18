771425c17e5cc5c29f1eb78fe39b4fb678449cb7
Jason Shiffler <jasonshiffler@yahoo.com>
2020-04-18 16:35:52 -0500

    -Added volatile keyword example

--- src/main/java/BasicThreadSynch.java
+++ src/main/java/BasicThreadSynch.java
@@ -1,4 +1,46 @@
 package main.java;
 
+import java.util.Scanner;
+
+class Processor extends Thread {
+
+    private volatile boolean running = true; // Prevents Threads from caching variables when
+                                             // they aren't changed from that thread
+
+
+    public void run() {
+
+        while (running) {
+
+            System.out.println("Hello!");
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } //close while
+
+    }//close method
+
+    public void shutdown(){
+        running = false;
+    }
+
+}
+
 public class BasicThreadSynch {
+
+    public static void main(String[] args) {
+        Processor proc1 = new Processor();
+        proc1.start();
+
+        System.out.println("Press return to stop");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        proc1.shutdown();
+
+    }
+
+
 }
